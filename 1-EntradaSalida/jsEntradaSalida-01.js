dcf3e3048694eda9787de8fb3fe39141f9ae9001
Your Name <you@example.com>
2020-07-13 19:39:18 -0300

    Segundo Commit

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,6 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Hola buenas noches");
+	alert("Bienvenido 1ro F");
 }
 
